b65bebbe4a5486b7f94bff0a1cd114aa060ef86e
Jessie <jessie.lai@bitzesty.com>
2015-08-10 18:11:49 +0100

    change homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :subscriptions
-  resources :assignments
+  root 'home#index'
+
   resources :locations
   resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
