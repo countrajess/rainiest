30763260443a0334d41eaf2fe0d817b0ac6d3335
Jessie <jessie.lai@bitzesty.com>
2015-08-10 16:08:11 +0100

    fix typo in migration

--- db/migrate/20150804172604_add_location_id_to_users.rb
+++ db/migrate/20150804172604_add_location_id_to_users.rb
@@ -1,6 +1,6 @@
 class AddLocationIdToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :location_id, :integers
+    add_column :users, :location_id, :integer
     remove_column :users, :user_id
   end
 end
